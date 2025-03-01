475278133c7b69ac493bf54b9b7e3ea12b1fbf79
jomi087 <jomi>
2025-03-01 23:10:21 +0530

    Upgraded Header With Sign Out Logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
+
 import  netflixLogo from '../assets/Netflix-logo.png'
+import  UserInfo from '../assets/profile_img.png'
+import { auth } from '../utils/firebase'
+
+import {signOut } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 
 const Header = () => {
+
+  const navigate = useNavigate()
+
+  const handleSignOutButton = ()=>{
+    console.log("clicked");
+    signOut(auth).then(() => {
+      navigate("/");
+    }).catch((error)=>{
+       toast.error("Failed to sign out. Please try again."); 
+    })
+      
+  } 
   return (
     <>
-      <div className="absolute px-40 py-7 bg-gradient-to-b from-black z-10">
+      <div className="absolute px-10 py-6 bg-gradient-to-b from-black z-10 w-screen flex justify-between">
           <img 
             src={netflixLogo} 
-            className="w-40"
+            className="w-35"
             alt="logo-Netflix"
             />
+        <div className='flex p-2'>
+          <img className='w-8 h-8 m-1'  src={UserInfo} alt="profileIcon" />
+          <button className='font-bold text-white' onClick={handleSignOutButton}>(Sign Out)</button>
+        </div>
       </div>
 
     </>
--- src/page/Browse.jsx
+++ src/page/Browse.jsx
@@ -1,32 +1,25 @@
 import React, { useContext, useEffect } from 'react'
-import {signOut } from "firebase/auth";
-import { auth } from '../utils/firebase';
+
 import '../index.css';
 import AuthContext from "../context/AuthContext";
 import { useNavigate } from 'react-router-dom';
+import Header from '../components/Header';
 
 
 const Browse = () => {
-  const { authUser, logout } = useContext(AuthContext);
+  const { authUser } = useContext(AuthContext);
   const navigate = useNavigate()
 
   useEffect(() => {
     if (!authUser) {
-      navigate('/'); // Redirect to login if not authenticated
+      navigate('/');  // Redirect to login if not authenticated
     }
-  }, [authUser, navigate]);
+  },[authUser, navigate]);
 
-  const handleSignOutButton = ()=>{
-    console.log("clicked");
-    signOut(auth).then(() => {
-      navigate("/");
-    });
-     
-  } 
   return (
     <>
-      <h1 className="text-red-700">hlo</h1>
-      <button  onClick={handleSignOutButton}>button</button>
+      <Header/>
+     <p>list of movies .....</p>
     </>
 
   )
