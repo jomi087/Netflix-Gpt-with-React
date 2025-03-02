90e136b50d43962faec552f82f44285e012aec54
jomi087 <jomi>
2025-03-02 12:17:48 +0530

    display signOut button   for authenticatedUser(login user)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import  netflixLogo from '../assets/Netflix-logo.png'
 import  UserInfo from '../assets/profile_img.png'
 import { auth } from '../utils/firebase'
+import AuthContext from '../context/AuthContext'
 
 import {signOut } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 
+
+
 const Header = () => {
 
   const navigate = useNavigate()
+  const {authUser} = useContext(AuthContext)
 
   const handleSignOutButton = ()=>{
     console.log("clicked");
@@ -29,11 +33,13 @@ const Header = () => {
             src={netflixLogo} 
             className="w-35"
             alt="logo-Netflix"
-            />
-        <div className='flex p-2'>
-          <img className='w-8 h-8 m-1'  src={UserInfo} alt="profileIcon" />
-          <button className='font-bold text-white' onClick={handleSignOutButton}>(Sign Out)</button>
-        </div>
+          />
+        { authUser && 
+          <div className='flex p-2'>
+            <img className='w-8 h-8 m-1'  src={UserInfo} alt="profileIcon" />
+            <button className='font-bold text-white' onClick={handleSignOutButton}>(Sign Out)</button>
+          </div>
+        }
       </div>
 
     </>
